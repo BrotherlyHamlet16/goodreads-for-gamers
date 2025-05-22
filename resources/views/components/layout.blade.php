3250786996cc4d43833729f2ae90b4231e3d84fa
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-22 16:31:31 +0300

    working on the nav bar

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlayOn</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <header>
+            <p>Big Games of Summer </p>
+            <p>Discover gamers' most anticipated new titles</p>
+        </header>
+        <nav>
+            <div>Logo</div>
+            <div>
+                <a href="">Home</a>
+                <a href="">My Games</a>
+                <a href="">Browse</a>
+                <a href="">Community</a>
+            </div>
+            <div>Search Bar</div>
+            <div>Social icons</div>
+        </nav>
+    </div>
+</body>
+</html>
