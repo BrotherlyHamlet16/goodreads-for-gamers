97c573425d42463597e8a14b80017180a42134b9
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-23 16:05:28 +0300

    searchbar

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,5 +1,6 @@
 <div class="flex items-center">
-    <form action="">
-        <input type="text" placeholder="Search games here..." aria-label="Search games here..." class="rounded bg-white/5 border-white/10 px-1 py-1 w-100%">
+    <form action="" class="w-full">
+        <input type="text" placeholder="Search games here..." aria-label="Search games here..." 
+        class="rounded bg-white/5 border-white/10 px-1 py-1 w-full text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
     </form>
 </div>
