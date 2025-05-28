141951026ed962261ce66ba12e18383355e56987
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-28 14:15:17 +0300

    defining sections

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,5 +17,26 @@
             {{ $slot }}
         </x-navbar>
     </div>
+
+    <div class="flex justify-between items-start p-4">
+        <section class="w-64 px-4 bg-amber-300 w-300px float-left m-r-2">
+            <div>Currently Playing</div>
+            <div>2025 Gamers Challenge</div>
+            <div>Want to Play</div>
+            <div>Shelves</div>
+        </section>
+
+        <section class="flex-grow px-8 items-center bg-red-500">
+            <div>Ths is a Banner</div>
+            <div>Updates</div>
+        </section>
+
+        <section class="w-64 px-4 bg-blue-600">
+            <div>Gaming News</div>
+            <div>Recommendations</div>
+            <div>Players Choice Awards</div>
+            <div>About Us Stuff</div>
+        </section>     
+    </div>
 </body>
 </html>
