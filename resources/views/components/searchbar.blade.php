fc78fc2192b90e1a3f081891cadc9ccd6881629c
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-28 13:21:47 +0300

    Search bar done

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,6 +1,12 @@
-<div class="flex items-center">
-    <form action="" class="w-full">
+<div class="flex-grow px-4">
+    <form action="" class="w-full relative">
         <input type="text" placeholder="Search games here..." aria-label="Search games here..." 
         class="rounded bg-white/5 border-white/10 px-1 py-1 w-full text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+        {{-- <div class="absolute inset-y-0 right-2 flex items-center pointer-events-none text-gray-400">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div> --}}
+        <span class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
+            <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+        </span>
     </form>
 </div>
