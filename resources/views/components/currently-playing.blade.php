8f829c86af88fb8b5b02cbf3e75a54663788968c
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-05-30 13:18:17 +0300

    currently playing card

---
+++ resources/views/components/currently-playing.blade.php
@@ -0,0 +1,28 @@
+<section class="max-w-xl mx-auto p-4 border border-gray-300 rounded-md bg-gray-50 font-sans">
+    <div class="mb-4">
+        <h3 class="text-xl mb-4 text-gray-800">Currently Playing</h3>
+        <div flex mb-4>
+            <a href="">
+                <img src="" alt="RDR2" class="w-30 h-auto mr-4 rounded">
+            </a>
+            <div class="flex-1 flex flex-col justify-between"> 
+                <div class="text-blue-600 hover:underline">
+                    <a href="">Red Dead Redemption 2</a>
+                </div>
+                <div class="text-blue-600 hover:underline">
+                    by <a href="">Rockstar Games</a>
+                </div>
+                <div class="mt-2">
+                    <button class="px-3 py-1 bg-blue-600 text-white text-sm rounded cursor-pointer">Update Progress</button>
+                </div>          
+            </div>
+        </div>
+        <div class="text-sm text-gray-600 mt-2">
+            <a href="" class="text-blue-600 hover:underline">View all games</a>
+            <span></span>
+            <button class="bg-transparent text-blue-600 text-sm hover:underline cursor-pointer" onclick="addGame()">Add a game</button>
+            <span></span>
+            <button class="bg-transparent text-blue-600 text-sm hover:underline cursor-pointer">General Update</button>
+        </div>
+    </div>
+</section>
--- resources/views/components/homeSecondaryColumn.blade.php
+++ resources/views/components/homeSecondaryColumn.blade.php
@@ -1,5 +1,8 @@
 <div class="w-64 px-4 bg-amber-300 w-300px float-left ml-5px mr-5px mt-12px">
-    <section>Currently Playing</section>
+    <x-currently-playing>
+        {{ $slot }}
+    </x-currently-playing>
+    
     <section>2025 Gamers Challenge</section>
     <section>Want to Play</section>
     <section>Shelves</section>
